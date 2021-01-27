08f773b2e25a56451be31dc4a2431aebb338384d
hexu <hexu_1985@sina.com>
2021-01-27 14:18:02 +0800

    update chapter-02/recipe-01

--- chapter-02/recipe-01/src/test.cpp
+++ chapter-02/recipe-01/src/test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-int Add(int a, int b)  
+inline int Add(int a, int b)  
 {  
     return a + b;  
 }  
@@ -16,5 +16,68 @@ TEST(fun, Add)
     ASSERT_EQ(6, Add(10,4)) << "assert test failure!"; // 运算结果不等于期望值, 使用ASSERT_EQ
 
     EXPECT_EQ(7, Add(2,4)) << "expect test failure!"; // 不会运行到这
+}  
+
+#if 0
+void test_add1()
+{
+    std::cout << "test_add1" << std::endl;
+    EXPECT_EQ(1, Add(2,-1));
+
+    EXPECT_EQ(5, Add(2,3));  
+
+    EXPECT_EQ(4, Add(2,4)); // 运算结果不等于期望值
+
+    ASSERT_EQ(6, Add(10,4)) << "assert test failure!"; // 运算结果不等于期望值, 使用ASSERT_EQ
+
+    EXPECT_EQ(7, Add(2,4)) << "expect test failure!"; // 不会运行到这
+}
+
+void test_add2()
+{
+    std::cout << "test_add2" << std::endl;
+    EXPECT_EQ(1, Add(2,-1));
+
+    EXPECT_EQ(5, Add(2,3));  
+
+    EXPECT_EQ(4, Add(2,4)); // 运算结果不等于期望值
+
+    ASSERT_EQ(6, Add(10,4)) << "assert test failure!"; // 运算结果不等于期望值, 使用ASSERT_EQ
+
+    EXPECT_EQ(7, Add(2,4)) << "expect test failure!"; // 不会运行到这
+}
+
+void test_add3()
+{
+    std::cout << "test_add3" << std::endl;
+    EXPECT_EQ(1, Add(2,-1));
+
+    EXPECT_EQ(5, Add(2,3));  
+
+    EXPECT_EQ(4, Add(2,4)); // 运算结果不等于期望值
+
+    ASSERT_EQ(6, Add(10,4)) << "assert test failure!"; // 运算结果不等于期望值, 使用ASSERT_EQ
+
+    EXPECT_EQ(7, Add(2,4)) << "expect test failure!"; // 不会运行到这
+}
+
+TEST(fun, Addfuncs)
+{
+    test_add1();
+    test_add2();
+    test_add3();
+}
+#endif
+
+inline int Sub(int a, int b)
+{
+    return a - b;
+}
+
+TEST(fun, Sub)  
+{  
+    EXPECT_EQ(3, Sub(2,-1));
+
+    EXPECT_EQ(-1, Sub(2,3));  
 }  
 
