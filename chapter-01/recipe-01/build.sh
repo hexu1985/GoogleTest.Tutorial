014759038e731eb117e1645af04177636cfec051
xu.he <xu.he@phigent.ai>
2025-03-04 15:13:44 +0800

    update chapter-01/recipe-01

--- chapter-01/recipe-01/build.sh
+++
@@ -1,5 +0,0 @@
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/local/googletest \
-    ..
-cmake --build . --target install
---
+++ chapter-01/recipe-01/build_by_cmake.sh
@@ -0,0 +1,3 @@
+cmake -S . -B build
+cmake --build build -j 4
+cmake --install build --prefix ~/local/googletest
